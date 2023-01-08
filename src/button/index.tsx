d95c1cec011c7cdae9f7e65902e110f0ae777501
令狐少侠 <xxx@yy.com>
2023-01-09 05:06:50 +0800

    refactor: button

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -3,30 +3,42 @@ import classNames from 'classnames';
 
 import './index.scss';
 
-interface buttonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface BaseProps {
 	className?: string;
 	type?: 'normal' | 'primary' | 'dashed' | 'link' | 'text';
 	size?: 'small' | 'medium' | 'large';
 	children: React.ReactNode;
 	style?: React.CSSProperties;
-	onClick?: () => void;
-	onBlur?: () => void;
 	htmlType?: 'button' | 'submit' | 'reset';
+	href?: string;
+	disabled?: boolean;
 }
+type NativeButtonProps = BaseProps & React.HTMLAttributes<HTMLButtonElement>;
+type AnchorButtonProps = BaseProps & React.HTMLAttributes<HTMLAnchorElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
-const Button = (props: buttonProps) => {
-	const { className, type = 'normal', size = 'medium', children, style, onClick, onBlur, htmlType = 'button', ...others } = props;
+const Button = (props: ButtonProps) => {
+	const { className, type = 'normal', size = 'medium', children, style, htmlType = 'button', href, disabled, ...restProps } = props;
 
 	const cls = classNames('ant-btn', className, {
 		[`ant-btn-${size}`]: size,
 		[`ant-btn-${type}`]: type,
+		disabled: type === 'link' && disabled,
 	});
 
-	return (
-		<button type={htmlType} className={cls} style={style} onClick={onClick} onBlur={onBlur} {...others}>
-			{children}
-		</button>
-	);
+	if (type === 'link' && href) {
+		return (
+			<a className={cls} style={style} href={href} {...restProps}>
+				{children}
+			</a>
+		);
+	} else {
+		return (
+			<button type={htmlType} className={cls} style={style} {...restProps}>
+				{children}
+			</button>
+		);
+	}
 };
 
 export default Button;
