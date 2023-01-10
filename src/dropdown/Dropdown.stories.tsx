d5dcd32a62064eaf91ad7b2c0dd89ebb24bbaa35
令狐少侠 <xxx@yy.com>
2023-01-11 02:04:03 +0800

    feat: Dropdown

---
+++ src/dropdown/Dropdown.stories.tsx
@@ -0,0 +1,98 @@
+import React, { ReactNode } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Dropdown from './Dropdown';
+import Menu from '../menu';
+import Button from '../button';
+
+export default {
+	title: 'Example/Dropdown',
+	component: Dropdown,
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+	overlay: <span>test</span>,
+	children: <span>Dropdown</span>,
+};
+
+const menu = (
+	<Menu
+		items={[
+			{
+				key: '1',
+				label: (
+					<a target='_blank' rel='noopener noreferrer' href='https://www.antgroup.com'>
+						1st menu item
+					</a>
+				),
+			},
+			{
+				key: '2',
+				label: (
+					<a target='_blank' rel='noopener noreferrer' href='https://www.aliyun.com'>
+						2nd menu item (disabled)
+					</a>
+				),
+				icon: <span>✅</span>,
+				disabled: true,
+			},
+			{
+				key: '3',
+				label: (
+					<a target='_blank' rel='noopener noreferrer' href='https://www.luohanacademy.com'>
+						3rd menu item (disabled)
+					</a>
+				),
+				disabled: true,
+			},
+			{
+				key: '4',
+				danger: true,
+				label: 'a danger item',
+			},
+		]}
+	/>
+);
+
+export const App: React.FC = () => (
+	<Dropdown overlay={menu}>
+		<button onClick={(e) => e.preventDefault()}>
+			Hover me
+			<span>✅</span>,
+		</button>
+	</Dropdown>
+);
+
+const Space = (props: any) => {
+	return <div style={{ display: 'flex', gap: 8 }}>{props.children}</div>;
+};
+
+export const Placement: React.FC = () => (
+	<Space direction='vertical'>
+		<Space wrap>
+			<Dropdown overlay={menu} placement='bottomLeft'>
+				<Button>bottomLeft</Button>
+			</Dropdown>
+			<Dropdown overlay={menu} placement='bottom'>
+				<Button>bottom</Button>
+			</Dropdown>
+			<Dropdown overlay={menu} placement='bottomRight'>
+				<Button>bottomRight</Button>
+			</Dropdown>
+		</Space>
+		<Space wrap>
+			<Dropdown overlay={menu} placement='topLeft'>
+				<Button>topLeft</Button>
+			</Dropdown>
+			<Dropdown overlay={menu} placement='top'>
+				<Button>top</Button>
+			</Dropdown>
+			<Dropdown overlay={menu} placement='topRight'>
+				<Button>topRight</Button>
+			</Dropdown>
+		</Space>
+	</Space>
+);
