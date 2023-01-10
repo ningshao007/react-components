9afa43eb42e827720d03a62172a1c98f1c727f43
令狐少侠 <xxx@yy.com>
2023-01-11 00:42:41 +0800

    feat: overylay

--- src/overlay/Overlay.tsx
+++ src/overlay/Overlay.tsx
@@ -1,21 +1,25 @@
-import React, { CSSProperties, ReactNode, useEffect, useState } from 'react';
+import React, { CSSProperties, useCallback, useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
-import classNames from 'classnames';
+import getPlacement, { PlacementType, PointsType } from './placement';
 
 export interface overlayProps extends React.HTMLAttributes<HTMLDivElement> {
 	className?: string;
-	children?: ReactNode;
+	children?: React.ReactElement;
 	hasMask?: boolean;
 	visible?: boolean;
 	onVisibleChange?: (e: boolean) => void;
 	style?: CSSProperties;
+	target?: HTMLElement | (() => HTMLElement);
+	points?: PointsType;
+	placement?: PlacementType;
+	beforePosition?: Function;
 }
 
 const Overlay = (props: overlayProps) => {
-	const { className, children, style, hasMask, visible: pVisible, onVisibleChange, ...others } = props;
+	const { className, children, style, hasMask, visible: pVisible, target, onVisibleChange, points, placement, beforePosition, ...others } = props;
 	const [visible, setVisible] = useState(pVisible || false);
-
-	const content = ReactDOM.createPortal(children, document.body);
+	const [positionStyle, setPositionStyle] = useState({});
+	const overlayRef = useRef<HTMLElement | null>(null);
 
 	useEffect(() => {
 		if ('visible' in props) {
@@ -24,28 +28,88 @@ const Overlay = (props: overlayProps) => {
 	}, [pVisible, props]);
 
 	useEffect(() => {
-		function handleMouseDown(e: MouseEvent) {
-			onVisibleChange?.(false);
-		}
+		const handleMouseDown = (e: MouseEvent) => {
+			const safeNodeList: Node[] = [];
+			const clickNode = e.target as Node;
 
-		if (visible) {
-			window.addEventListener('mousedown', handleMouseDown, false);
-		}
+			if (overlayRef.current) {
+				safeNodeList.push(overlayRef.current);
+			}
+
+			for (let i = 0; i < safeNodeList.length; i++) {
+				const node = safeNodeList[i];
+				if (node && node.contains(clickNode)) {
+					return;
+				}
+			}
+
+			onVisibleChange?.(false);
+		};
+
+		window.addEventListener('mousedown', handleMouseDown);
 
 		return () => {
-			window.removeEventListener('mousedown', handleMouseDown, false);
+			window.removeEventListener('mousedown', handleMouseDown);
+		};
+	}, [onVisibleChange]);
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (!visible || !overlayRef.current) {
+				return;
+			}
+			if (e.key === 'Escape') {
+				onVisibleChange?.(false);
+			}
+		};
+
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
 		};
 	}, [onVisibleChange, visible]);
+
+	const overlayRefCallback = useCallback(
+		(node: HTMLElement) => {
+			if (!node) return;
+
+			overlayRef.current = node;
+
+			if (node && target) {
+				const targetElement = typeof target === 'function' ? target() : target;
+				const positionStyle = getPlacement({
+					target: targetElement,
+					overlay: node,
+					points,
+					placement,
+					beforePosition,
+				});
+
+				setPositionStyle(positionStyle);
+			}
+		},
+		[beforePosition, placement, points, target],
+	);
+
+	const child = React.Children.only(children)!;
+
+	const newChildren = React.cloneElement(child, {
+		...others,
+		ref: overlayRefCallback,
+		style: { ...child?.props?.style, ...positionStyle },
+	});
 
 	if (!visible) {
 		return null;
 	}
 
-	return (
+	return ReactDOM.createPortal(
 		<div>
 			{hasMask ? <div /> : null}
-			{content}
-		</div>
+			{newChildren}
+		</div>,
+		document.body,
 	);
 };
 
